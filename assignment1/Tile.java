41f21ae600687843477ee70df279cad5f46971eb
bepissbepisss <61n6xvwz@duck.com>
2026-02-08 21:32:40 -0500

    work

---
+++ assignment1/Tile.java
@@ -0,0 +1,18 @@
+// Matt L'Ecuyer 260454319
+
+package assignment1
+
+public class Tile {
+
+	private Tile position;
+	private double health;
+	private int weaponType;
+	private int attackDamage;
+
+	public Tile(Tile t, double hp, int wp, int ad) {
+		position = t;
+
+	}
+
+}
+
