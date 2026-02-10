7bfad6e884dc0d02e6f79d8148ed15f510f52530
bepissbepisss <118068430+bepissbepisss@users.noreply.github.com>
2026-02-09 20:17:04 -0500

    p3 monster

--- assignment1/Monster.java
+++ assignment1/Monster.java
@@ -12,12 +12,43 @@ public class Monster extends Fighter{
 //	}
 
 	public int takeAction(){
-		if (this.getPosition().getWarrior() != null) {
+		// if there is a warrior on this tile
+		Tile currentTile = this.getPosition();
+		if (currentTile.getWarrior() != null) {
 			// do damage to warrior
-			this.getPosition().getWarrior().takeDamage(this.getAttackDamage(), this.getWeaponType());
+			currentTile.getWarrior().takeDamage(this.getAttackDamage(), this.getWeaponType());
+			// now go to the back of the troop.
+			currentTile.removeFighter(this);
+			currentTile.addFighter(this);
+		} else { //advance towards the castle
+			Tile nextTile = currentTile.towardTheCastle();
+			currentTile.removeFighter(this);
+			nextTile.addFighter(this);
+
+			this.setPosition(nextTile);
+				
 		}
 		return 0;
 		
+	}
+
+	public boolean equals(Object o) {
+		if (!(o instanceof Monster)) {
+			return false;
+		}
+		Monster other = (Monster) o;
+
+		if (super.equals(other)) {
+			return true;
+		} else {
+			return false;
+		}
+
+//		if (other.getPosition() == this.getPosition() && (other.getHealth() <= this.getHealth() + 0.001 && other.getHealth() >= this.getHealth()- 0.001) &&  other.getAttackDamage() == this.getAttackDamage()) {
+//			return true;
+//		} else {
+//			return false;
+//		}
 	}
 
 	public Monster(Tile t, double hp, int wp, int ad) {
--- assignment1/MonsterTroop.java
+++ assignment1/MonsterTroop.java
@@ -61,11 +61,11 @@ public class MonsterTroop {
 	}
 
 
-	// i believe this should remove from the back
+	// i believe this should remove from the front
 	public boolean removeMonster(Monster m){
 		// find the first occurence from the back
 		int index = -1;
-		for(int i=numOfMonsters-1;i !=0 ; i--) {
+		for(int i=0;i < numOfMonsters; i++) {
 			if (monsters[i] == m) {
 				index = i;
 //				System.out.println("Found match at index " + index);
@@ -113,7 +113,7 @@ public class MonsterTroop {
 	void main(String[] Args) {
 		MonsterTroop mt = new MonsterTroop();
 		Tile t = new Tile();
-		Monster m = new Monster(t, 11.002,10,10);
+		Monster m = new Monster(t, 11.001,11,10);
 		Monster n = new Monster(t, 11,10,10);
 		Monster s = new Monster(t, 12,1,1);
 		Monster a = new Monster(t, 11,10,10);
@@ -129,20 +129,21 @@ public class MonsterTroop {
 
 		boolean change;
 		change = mt.removeMonster(n);
-	
-		System.out.println("Size is " + mt.sizeOfTroop());
-		System.out.println(mt);
-		System.out.println("Change? " +change);
-		
-		Monster[] flip = flipList(mt);
-		for (int i=0; i<flip.length; i++){
-			System.out.println(flip[i]);
-		}
-
+//	
+//		System.out.println("Size is " + mt.sizeOfTroop());
+//		System.out.println(mt);
+//		System.out.println("Change? " +change);
+//		
+//		Monster[] flip = flipList(mt);
+//		for (int i=0; i<flip.length; i++){
+//			System.out.println(flip[i]);
+//		}
+//
+		Warrior w = new Warrior(t,11.001,11,10,1);
 		System.out.println("are they the same? " + m.equals(n));
-
-		n.takeDamage(100,1);
-		System.out.println(mt);
+//
+//		n.takeDamage(100,1);
+//		System.out.println(mt);
 
 	}
 
