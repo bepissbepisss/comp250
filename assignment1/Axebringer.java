cbc9eaeca78544bccd33427ea8c68d87834ed4f1
bepissbepisss <118068430+bepissbepisss@users.noreply.github.com>
2026-02-11 21:29:49 -0500

    cooldown

--- assignment1/Axebringer.java
+++ assignment1/Axebringer.java
@@ -8,9 +8,11 @@ public class Axebringer extends Warrior {
 	public static int BASE_COST;
 	public static int WEAPON_TYPE = 2; //axe
 	public static int BASE_ATTACK_DAMAGE;
+	private boolean cooldown = false;
 
 	public Axebringer(Tile t) {
 		super(t,BASE_HEALTH, WEAPON_TYPE, BASE_ATTACK_DAMAGE ,BASE_COST);
+		cooldown = false;
 	}
 
 
@@ -20,20 +22,25 @@ public class Axebringer extends Warrior {
 	}
 
 	public int takeAction(){
-		Tile currentTile = this.getPosition();
-		Tile nextTile = currentTile.towardTheCamp();
-		int skillPoints = 0;
-		int ad = this.getAttackDamage();
-		if (currentTile.getNumOfMonsters() != 0) {
-			double damageDealt = currentTile.getMonster().takeDamage(this.getAttackDamage(), this.getWeaponType());
-		skillPoints = (int) (((double) ad) / damageDealt +1);
-		} else if (nextTile.getNumOfMonsters() != 0 && nextTile.isCamp() == false) {
-			double damageDealt = nextTile.getMonster().takeDamage(this.getAttackDamage(), this.getWeaponType());
-		skillPoints = (int) (((double) ad) / damageDealt +1);
+		if (cooldown == false) {
+			Tile currentTile = this.getPosition();
+			Tile nextTile = currentTile.towardTheCamp();
+			int skillPoints = 0;
+			int ad = this.getAttackDamage();
+			if (currentTile.getNumOfMonsters() != 0) {
+				double damageDealt = currentTile.getMonster().takeDamage(this.getAttackDamage(), this.getWeaponType());
+				skillPoints = (int) (((double) ad) / damageDealt +1);
+			} else if (nextTile.getNumOfMonsters() != 0 && nextTile.isCamp() == false) {
+				cooldown = true;
+				double damageDealt = nextTile.getMonster().takeDamage(this.getAttackDamage(), this.getWeaponType());
+				skillPoints = (int) (((double) ad) / damageDealt +1);
+			}
+
+
+			return skillPoints;
 		}
-
-
-		return skillPoints;
+		cooldown = false;
+		return 0;
 
 	}
 
